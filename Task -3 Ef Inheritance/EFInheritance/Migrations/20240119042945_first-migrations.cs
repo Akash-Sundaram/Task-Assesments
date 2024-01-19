a05b78551a5169323af6bda1746c884f08b31cf6
Akash Sundaram <akash.sundaram@aspiresys.com>
2024-01-19 15:48:21 +0530

    tasks

---
+++ Task -3 Ef Inheritance/EFInheritance/Migrations/20240119042945_first-migrations.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFInheritance.Migrations
+{
+    /// <inheritdoc />
+    public partial class firstmigrations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
